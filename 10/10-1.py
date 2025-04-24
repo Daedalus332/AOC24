694b8ba4bc31ae01cf6d9c97c6953c93187a0130
daedalus332 <daedalus332@gmail.com>
2025-04-24 22:36:07 +0100

    10-1 and 10-2 done baybeee

--- 10/10-1.py
+++ 10/10-1.py
@@ -1,37 +1,72 @@
 trailheadcoords = []
 mapp = []
+directions = []
+tempdir = []
 xcount = 0
 ycount = 0
+total = 0
+trailtotal = 0
+
+open("out.txt", "w").close()
 
 def scanSurroundings(coords):
+	global trailtotal
+	global total
+	global directions
 	current = mapp[coords[0]][coords[1]]
+	if current == 0:
+		directions = []
+		#with open("out.txt", "a") as f:
+		#	f.write(','.join(str(x) for x in coords) + '\n')
+		#	f.close()
 	if current == 9:
-		total += 1
+		if coords not in directions:
+			directions.append(coords)
+			total += 1
+		#with open("out.txt", "a") as f:
+		#	f.write(' '.join(directions) + '\n')
+		#	f.close()
 		return
 	else:
 		target = current + 1
 		if coords[0] - 1 >= 0:
 			if target == mapp[coords[0]-1][coords[1]]:
-				scanSurroundings[]
+				#tempdir = directions.copy()
+				scanSurroundings([coords[0]-1, coords[1]])
+				#directions = tempdir.copy()
 		if coords[1] - 1 >= 0:
-		if coords[0] + 1 <= xcount:
-		if coords[1] -1 <= ycount:
+			if target == mapp[coords[0]][coords[1]-1]:
+				#tempdir = directions.copy()
+				scanSurroundings([coords[0], coords[1]-1])
+				#directions = tempdir.copy()
+		if coords[0] + 1 < ycount:
+			if target == mapp[coords[0]+1][coords[1]]:
+				#tempdir = directions.copy()
+				scanSurroundings([coords[0]+1, coords[1]])
+				#directions = tempdir.copy()
+		if coords[1] + 1 < xcount:
+			if target == mapp[coords[0]][coords[1]+1]:
+				#tempdir = directions.copy()
+				scanSurroundings([coords[0], coords[1]+1])
+				#directions = tempdir.copy()
 
-with open("test.txt", "r") as f:
+with open("input.txt", "r") as f:
 	for line in f:
 		linesplit = list(line)
 		if '\n' in linesplit:
 			linesplit.remove('\n')
+		linesplit = list(map(int, linesplit))
 		mapp.append(linesplit)
 
 for line in mapp:
 	xcount = 0
 	for item in line:
-		if item == "0":
-			trailheadcoords.append([xcount, ycount])
+		if item == 0:
+			trailheadcoords.append([ycount, xcount])
 		xcount += 1
 	ycount += 1
 
 for trailhead in trailheadcoords:
 	scanSurroundings(trailhead)
 
+print(total)
